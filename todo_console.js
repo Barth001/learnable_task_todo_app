4caeb034804fd73178a5e2187816a14d738338f7
Millimeter-Wave <alaekwebartholomew@gmail.com>
2023-01-18 10:34:39 -0800

    Added date, display and user input

--- todo_console.js
+++ todo_console.js
@@ -1,14 +1,36 @@
+const prompt = require("prompt-sync")({ sigint: true });
 class TodoConsoleApp {
 
     constructor(){
-        
+        this.dataBase = []
+    }
+
+    currentDate(){
+        const options = {  weekday: 'short', day: 'numeric', month: 'long'};
+        const prnDt = new Date().toLocaleDateString('en-uk', options);
+        return prnDt
     }
 
     entrance(){}
     
-    addItem(){}
+    addItem(){
+        console.log("Add your daily activity: ");
+        let input = prompt()
+        this.dataBase.push(input)
+        this.display()
+    }
     
     updateItem(){}
     
     deleteItem(){}
+
+    display(){
+        this.dataBase.forEach((element,index) => {
+            console.log(this.currentDate());
+            console.log(`| ${index} | ${element} |`);
+        });
+    }
 }
+
+todo = new TodoConsoleApp();
+todo.addItem()
